fd928ad494bd54375570af48a034a00d300d507e
Mateo Congo <aidenperse@gmail.com>
2023-10-06 10:06:41 -0500

    conclusion proyect_1

--- Hola mundo/src/App.jsx
+++ Hola mundo/src/App.jsx
@@ -1,15 +1,54 @@
 import './App.css'
 import { TwitterFollowCart } from './TwitterFollowCard'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: true
+    },
+    {
+        userName: 'martindev',
+        name: 'Antonio Hermez Perez',
+        isFollowing: true
+    },
+    {
+        userName: 'Rubio5',
+        name: 'El rubius',
+        isFollowing: true
+    },
+    {
+        userName: 'esfera_iphone',
+        name: 'Esfera de la fortuna',
+        isFollowing: true
+    },
+    {
+        userName: 'AoTJewels',
+        name: 'Attack on Titan',
+        isFollowing: true
+    },
+    {
+        userName: 'JJKcontents',
+        name: 'Jujutsu Kaisen',
+        isFollowing: true
+    }
+]
 export function App (){
     return(
         <section className='App'>
-            <TwitterFollowCart userName="martindev" name="Antonio Hermez Perez" />
-            <TwitterFollowCart userName="midudev" name="Miguel Ángel Durán" />
-            <TwitterFollowCart userName="Rubio5" name="El rubius" />
-            <TwitterFollowCart userName="esfera_iphone" name="Esfera de la fortuna" />
-            <TwitterFollowCart userName="AoTJewels" name="Attack on Titan" />
-            <TwitterFollowCart userName="JJKcontents" name="Jujutsu Kaisen" />
+           {
+           users.map(({userName, name, isFollowing}) =>{
+                return(
+                    <TwitterFollowCart 
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                    >    
+                        {name}
+                    </TwitterFollowCart>
+                )
+            })
+            }
         </section>
     )
 }
--- Hola mundo/src/TwitterFollowCard.jsx
+++ Hola mundo/src/TwitterFollowCard.jsx
@@ -1,20 +1,33 @@
-export function TwitterFollowCart ({userName, name, isFollowing}){
+import { useState } from "react"
+
+export function TwitterFollowCart ({children,userName}){
+    const [isFollowing, setIsFollowing] = useState(false)
+
+    const text = isFollowing ? 'Siguiendo': 'Seguir'
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button is-following'
+        : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return(
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-avatar'src={`https://unavatar.io/${userName}`} alt="Avatar"></img>
                 <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
+                    <strong>{children}</strong>
                     <span className='tw-followCard-infoUserName'>@{userName}</span>
                 </div>
             </header>
             
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className="tw-followCard-text">{text}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
                 </button>
             </aside>
         </article>
     )
 }
-//sindresorhus@gmail.com
