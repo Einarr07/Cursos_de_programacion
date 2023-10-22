eed132e3cf4a159998fd0e777c3d4c95e72402a7
Mateo Congo <aidenperse@gmail.com>
2023-10-22 18:00:43 -0500

    cambios generales del hola mundo

--- Elementos_basicos/holaMundo.py
+++ Elementos_basicos/holaMundo.py
@@ -1,4 +1,27 @@
 print("Hola mundo")
 print("Mi nombre es Mateo")
-
 print("Este es un mensaje con un salto de linea\nsegunda parte xD")
+
+# Primer comentario en pyhton
+
+"""
+Pyton permite el tipado dinamico, en este caso cambio los valores de los números
+"""
+
+num1 = 345
+num2 = 234
+resutadoSuma = num1 + num2
+
+print("Este es el resultado de la suma: ", resutadoSuma)
+
+num1 = 789
+resultadoResta = num1 - num2
+print("Este es el resultado de la resta: ", resultadoResta)
+
+num1 = 8
+resultadoMul = num1 * num2
+print("Este es el resultado de la multiplicación: ", resultadoMul)
+
+num1 = 100
+resultadoDiv = num1 / num2
+print("Este es el resultado de la división: ", resultadoDiv)
