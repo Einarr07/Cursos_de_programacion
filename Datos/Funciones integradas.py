f3745be4b030fca05733ff582f8396352cdcb16d
Mateo Congo <aidenperse@gmail.com>
2023-10-24 11:26:35 -0500

    funciones integradas

---
+++ Datos/Funciones integradas.py
@@ -0,0 +1,30 @@
+# Funciones integradas
+
+# Convertir una cadena a un entero o flotante
+
+entero = int("56")
+flotante = float("45.67")
+
+# Convertir a una cadena
+
+cadena = str(45)
+
+# Entero a binario
+
+binario = bin(45)
+
+# Convertir a hexadecimal
+
+hexadecimal = hex(678)
+
+# Valor absoluto
+
+absoluto = abs(-89)
+
+# Redondear un numero
+
+redondear = round(78.6)
+
+# Cuenta el número de caracteres de la cadena
+
+caracteres = len("Juaquin")
