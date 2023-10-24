ece7ba8ae4e173f6416dc9c3f875b3d46226ed87
Mateo Congo <aidenperse@gmail.com>
2023-10-23 23:34:39 -0500

    Entrada y salida de datos

---
+++ Datos/Entrada de datos.py
@@ -0,0 +1,10 @@
+# Entrada de datos
+
+# Datos de tipo cadena
+nombre = input("Ingresa tu nombre: ")
+
+# Datos de tipo numericos enteros (int) y flotantes (float)
+numero = int(input("Ingresa un número: "))
+
+print(f"Hola {nombre}, el número que ingresaste es: {numero}")
+
---
+++ Datos/Salida de datos.py
@@ -0,0 +1,11 @@
+# Salida de datos por consola
+
+nombre = "Mateo"
+edad = 22
+
+print("Hola", nombre, "soy tu yo del pasado y tienes", edad, "años")
+
+print("Hola {} ¿cómo estas?, tienes {} años y estas a 1 semestre de terminar la universidad".format(nombre,edad))
+
+print(f"Hola {nombre} estas siguiendo el curso de programación de Python de 'Programación ATS' "
+      f"a la edad de {edad} años.")
