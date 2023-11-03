55d68c75b3b265fde77fc5c43772ad0a4615132a
Mateo Congo <aidenperse@gmail.com>
2023-11-02 19:36:46 -0500

    ejercicio3

---
+++ Colecciones/Ejercicios/ejercicio3.py
@@ -0,0 +1,28 @@
+
+personajes = []
+
+aragon = {
+    "nombre": "Aragon",
+    "clase": "Guerrero",
+    "raza": "Dúnadan del norte",
+}
+
+personajes.append(aragon)
+
+gandalf = {
+    "nombre": "Gandal",
+    "clase": "Mago",
+    "raza": "Islar",
+}
+
+personajes.append(gandalf)
+
+legolas = {
+    "nombre": "Legolas",
+    "clase": "Arquero",
+    "raza": "Dúnadan del norte",
+}
+
+personajes.append(legolas)
+
+print(personajes)
